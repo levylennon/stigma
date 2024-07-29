c878c96591ac4f3fde32828631f7bcdf93193f24
Aerafal <jimmy.mll@hotmail.com>
2024-07-30 01:08:11 +0200

    feat(protocol-builder): add some string extensions

--- tools/Stigma.Tools.ProtocolBuilder/Extensions/StringExtensions.cs
+++ tools/Stigma.Tools.ProtocolBuilder/Extensions/StringExtensions.cs
@@ -8,4 +8,30 @@ public static class StringExtensions
     {
         return Path.Combine(@namespace.Split('.', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Pascalize()).ToArray());
     }
+
+    public static string PascalizeWithSpecialChars(this string text)
+    {
+        if (text.Length > 2 && text[0] is '@')
+            return string.Concat(char.ToUpper(text[1]).ToString(), text.AsSpan(2));
+
+        return text.Pascalize();
+    }
+
+    public static string PascalizeNamespace(this string str)
+    {
+        return string.IsNullOrEmpty(str)
+            ? string.Empty
+            : string.Join('.', str.Split('.').Select(x => x.Pascalize()));
+    }
+
+    public static string ToValidName(this string s)
+    {
+        return s switch
+        {
+            "object" => $"@{s}",
+            "base" => $"@{s}",
+            "params" => $"@{s}",
+            _ => s
+        };
+    }
 }
