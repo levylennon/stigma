0eba9f15d93b7060c7fee8268286a7221e559d2e
Aerafal <jimmy.mll@hotmail.com>
2024-07-29 22:49:15 +0200

    feat(protocol-builder): add NamespaceToPath string extension

---
+++ tools/Stigma.Tools.ProtocolBuilder/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Humanizer;
+
+namespace Stigma.Tools.ProtocolBuilder.Extensions;
+
+public static class StringExtensions
+{
+    public static string NamespaceToPath(this string @namespace)
+    {
+        return Path.Combine(@namespace.Split('.', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Pascalize()).ToArray());
+    }
+}
