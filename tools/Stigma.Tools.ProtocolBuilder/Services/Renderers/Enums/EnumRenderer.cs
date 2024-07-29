6deb726bd766f48676ac63d01f3fc926055011cc
Aerafal <jimmy.mll@hotmail.com>
2024-07-29 22:53:17 +0200

    feat(protocol-builder): add signature on EnumRenderer

--- tools/Stigma.Tools.ProtocolBuilder/Services/Renderers/Enums/EnumRenderer.cs
+++ tools/Stigma.Tools.ProtocolBuilder/Services/Renderers/Enums/EnumRenderer.cs
@@ -1,13 +1,24 @@
-﻿using Stigma.Core.IO.Text;
+﻿using Microsoft.Extensions.Options;
+using Stigma.Core.IO.Text;
 using Stigma.Tools.ProtocolBuilder.Models.Symbols;
+using Stigma.Tools.ProtocolBuilder.Options;
 
 namespace Stigma.Tools.ProtocolBuilder.Services.Renderers.Enums;
 
 public sealed class EnumRenderer : IRenderer
 {
+    private readonly GlobalOptions _options;
+    
+    public EnumRenderer(IOptions<GlobalOptions> options)
+    {
+        _options = options.Value;
+    }
+    
     public string Render(ClassSymbol classSymbol)
     {
         var builder = new IndentedStringBuilder()
+            .AppendLine(_options.Signature)
+            .AppendLine()
             .AppendLine("namespace {0};", classSymbol.Type.Namespace)
             .AppendLine()
             .AppendLine("public enum {0}", classSymbol.Type.Name)
