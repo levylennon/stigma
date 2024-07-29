67e16562c9e5bd133a8c9d1a349fb67c06fc3742
Aerafal <jimmy.mll@hotmail.com>
2024-07-29 22:48:50 +0200

    feat(protocol-builder): add options

---
+++ tools/Stigma.Tools.ProtocolBuilder/Options/PathOptions.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+
+namespace Stigma.Tools.ProtocolBuilder.Options;
+
+public sealed class PathOptions
+{
+    public string Input { get; set; }
+
+    public string Output { get; set; }
+}
