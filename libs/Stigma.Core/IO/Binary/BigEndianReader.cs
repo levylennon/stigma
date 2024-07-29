71a5736cc53a6ea80ae869e5082122481810c2fa
Aerafal <jimmy.mll@hotmail.com>
2024-07-30 01:12:48 +0200

    feat(core): add big endian reader and writer

---
+++ libs/Stigma.Core/IO/Binary/BigEndianReader.cs
@@ -0,0 +1,145 @@
+﻿using System.Buffers.Binary;
+using System.Text;
+
+namespace Stigma.Core.IO.Binary;
+
+public sealed class BigEndianReader
+{
+    private readonly ReadOnlyMemory<byte> _buffer;
+
+    public int Length =>
+        _buffer.Length;
+
+    public int Position { get; private set; }
+
+    public int BytesAvailable =>
+        Length - Position;
+
+    public BigEndianReader(ReadOnlyMemory<byte> buffer)
+    {
+        _buffer = buffer;
+    }
+
+    public byte ReadUInt8()
+    {
+        return ReadSpan(sizeof(byte))[0];
+    }
+
+    public sbyte ReadInt8()
+    {
+        return (sbyte)ReadSpan(sizeof(byte))[0];
+    }
+
+    public bool ReadBoolean()
+    {
+        return ReadSpan(sizeof(byte))[0] is not 0;
+    }
+
+    public ushort ReadUInt16()
+    {
+        return BinaryPrimitives.ReadUInt16BigEndian(ReadSpan(sizeof(ushort)));
+    }
+
+    public short ReadInt16()
+    {
+        return BinaryPrimitives.ReadInt16BigEndian(ReadSpan(sizeof(short)));
+    }
+
+    public uint ReadUInt32()
+    {
+        return BinaryPrimitives.ReadUInt32BigEndian(ReadSpan(sizeof(uint)));
+    }
+
+    public int ReadInt32()
+    {
+        return BinaryPrimitives.ReadInt32BigEndian(ReadSpan(sizeof(int)));
+    }
+
+    public ulong ReadUInt64()
+    {
+        return BinaryPrimitives.ReadUInt64BigEndian(ReadSpan(sizeof(ulong)));
+    }
+
+    public long ReadInt64()
+    {
+        return BinaryPrimitives.ReadInt64BigEndian(ReadSpan(sizeof(long)));
+    }
+
+    public float ReadFloat()
+    {
+        return BinaryPrimitives.ReadSingleBigEndian(ReadSpan(sizeof(float)));
+    }
+
+    public double ReadDouble()
+    {
+        return BinaryPrimitives.ReadDoubleBigEndian(ReadSpan(sizeof(double)));
+    }
+
+    public ReadOnlyMemory<byte> ReadMemory(int count)
+    {
+        var memory = _buffer.Slice(Position, count);
+        Position += count;
+        return memory;
+    }
+
+    public ReadOnlyMemory<byte> ReadMemoryToEnd()
+    {
+        var memory = _buffer[Position..];
+        Position = _buffer.Length;
+        return memory;
+    }
+
+    public ReadOnlySpan<byte> ReadSpan(int count)
+    {
+        var span = _buffer.Span.Slice(Position, count);
+        Position += count;
+        return span;
+    }
+
+    public ReadOnlySpan<byte> ReadSpanToEnd()
+    {
+        var span = _buffer.Span[Position..];
+        Position = _buffer.Length;
+        return span;
+    }
+
+    public byte[] ReadBytes(int count)
+    {
+        var bytes = _buffer.Slice(Position, count).ToArray();
+        Position += count;
+        return bytes;
+    }
+
+    public byte[] ReadBytesToEnd()
+    {
+        var bytes = _buffer[Position..].ToArray();
+        Position = _buffer.Length;
+        return bytes;
+    }
+
+    public string ReadUtfBytes(int count)
+    {
+        return Encoding.UTF8.GetString(ReadSpan(count));
+    }
+
+    public string ReadUtf()
+    {
+        return ReadUtfBytes(ReadUInt16());
+    }
+
+    public string ReadBigUtf()
+    {
+        return ReadUtfBytes(ReadInt32());
+    }
+
+    public void Seek(SeekOrigin origin, int offset)
+    {
+        Position = origin switch
+        {
+            SeekOrigin.Begin => offset,
+            SeekOrigin.Current => Position + offset,
+            SeekOrigin.End => _buffer.Length - Math.Abs(offset),
+            _ => throw new ArgumentOutOfRangeException(nameof(origin))
+        };
+    }
+}
---
+++ libs/Stigma.Core/IO/Binary/BigEndianWriter.cs
@@ -0,0 +1,224 @@
+﻿using System.Buffers;
+using System.Buffers.Binary;
+using System.Text;
+
+namespace Stigma.Core.IO.Binary;
+
+public sealed class BigEndianWriter : IDisposable
+{
+    private byte[] _buffer = [];
+    private bool _isBufferRented;
+    private int _position;
+
+    public int MaxPosition { get; private set; }
+
+    public int Length =>
+        _buffer.Length;
+
+    public int Position
+    {
+        get => _position;
+        private set
+        {
+            _position = value;
+
+            if (MaxPosition < value)
+                MaxPosition = value;
+        }
+    }
+
+    public int BytesAvailable =>
+        Length - Position;
+
+    public Memory<byte> BufferAsMemory =>
+        _buffer.AsMemory(0, MaxPosition);
+
+    public Span<byte> BufferAsSpan =>
+        _buffer.AsSpan(0, MaxPosition);
+
+    public byte[] BufferAsArray =>
+        _buffer.AsSpan(0, MaxPosition).ToArray();
+
+    public void Dispose()
+    {
+        if (_isBufferRented)
+        {
+            ArrayPool<byte>.Shared.Return(_buffer);
+            _isBufferRented = false;
+        }
+    }
+
+    public void WriteUInt8(byte value)
+    {
+        GetSpan(sizeof(byte))[0] = value;
+    }
+
+    public void WriteInt8(sbyte value)
+    {
+        GetSpan(sizeof(sbyte))[0] = (byte)value;
+    }
+
+    public void WriteBoolean(bool value)
+    {
+        GetSpan(sizeof(bool))[0] = (byte)(value ? 1 : 0);
+    }
+
+    public void WriteUInt16(ushort value)
+    {
+        BinaryPrimitives.WriteUInt16BigEndian(GetSpan(sizeof(ushort)), value);
+    }
+
+    public void WriteInt16(short value)
+    {
+        BinaryPrimitives.WriteInt16BigEndian(GetSpan(sizeof(short)), value);
+    }
+
+    public void WriteUInt32(uint value)
+    {
+        BinaryPrimitives.WriteUInt32BigEndian(GetSpan(sizeof(uint)), value);
+    }
+
+    public void WriteInt32(int value)
+    {
+        BinaryPrimitives.WriteInt32BigEndian(GetSpan(sizeof(int)), value);
+    }
+
+    public void WriteUInt64(ulong value)
+    {
+        BinaryPrimitives.WriteUInt64BigEndian(GetSpan(sizeof(ulong)), value);
+    }
+
+    public void WriteInt64(long value)
+    {
+        BinaryPrimitives.WriteInt64BigEndian(GetSpan(sizeof(long)), value);
+    }
+
+    public void WriteFloat(float value)
+    {
+        BinaryPrimitives.WriteSingleBigEndian(GetSpan(sizeof(float)), value);
+    }
+
+    public void WriteDouble(double value)
+    {
+        BinaryPrimitives.WriteDoubleBigEndian(GetSpan(sizeof(double)), value);
+    }
+
+    public void WriteMemory(Memory<byte> value)
+    {
+        WriteSpan(value.Span);
+    }
+
+    public void WriteSpan(Span<byte> value)
+    {
+        value.CopyTo(GetSpan(value.Length));
+    }
+
+    public void WriteBytes(byte[] value)
+    {
+        WriteSpan(value);
+    }
+
+    public void WriteUtfBytes(string value)
+    {
+        WriteSpan(Encoding.UTF8.GetBytes(value));
+    }
+
+    public void WriteUtf(string value)
+    {
+        var bytes = Encoding.UTF8.GetBytes(value);
+
+        WriteUInt16((ushort)bytes.Length);
+        WriteSpan(bytes);
+    }
+
+    public void WriteBigUtf(string value)
+    {
+        var bytes = Encoding.UTF8.GetBytes(value);
+
+        WriteInt32(bytes.Length);
+        WriteSpan(bytes);
+    }
+
+    public void Seek(SeekOrigin origin, int offset)
+    {
+        switch (origin)
+        {
+            case SeekOrigin.Begin:
+                CheckAndResizeBuffer(offset, offset);
+                Position = offset;
+                break;
+            case SeekOrigin.Current:
+                CheckAndResizeBuffer(offset);
+                Position += offset;
+                break;
+            case SeekOrigin.End:
+                Position = Length - Math.Abs(offset);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(origin));
+        }
+    }
+
+    private Span<byte> GetSpan(int count)
+    {
+        CheckAndResizeBuffer(count);
+
+        var span = _buffer.AsSpan(Position, count);
+
+        Position += count;
+
+        return span;
+    }
+
+    private void CheckAndResizeBuffer(int count, int? position = null)
+    {
+        position ??= Position;
+
+        var bytesAvailable = Length - position.Value;
+
+        if (count <= bytesAvailable)
+            return;
+
+        var currentCount = Length;
+        var growBy = Math.Max(count, currentCount);
+
+        if (count is 0)
+            growBy = Math.Max(growBy, 256);
+
+        var newCount = currentCount + growBy;
+
+        if ((uint)newCount > int.MaxValue)
+        {
+            var needed = (uint)(currentCount - bytesAvailable + count);
+
+            if (needed > Array.MaxLength)
+                throw new OutOfMemoryException("The requested operation would exceed the maximum array length.");
+
+            newCount = Array.MaxLength;
+        }
+
+        var newArray = ArrayPool<byte>.Shared.Rent(newCount);
+        Array.Copy(_buffer, newArray, _buffer.Length);
+
+        if (_isBufferRented)
+            ArrayPool<byte>.Shared.Return(_buffer);
+
+        _buffer = newArray;
+        _isBufferRented = true;
+    }
+
+    public ReadOnlyMemory<byte> GetBufferAsMemory(int length)
+    {
+        return _buffer.AsMemory(0, length);
+    }
+
+    public ReadOnlySpan<byte> GetBufferAsSpan(int length)
+    {
+        return _buffer.AsSpan(0, length);
+    }
+
+    public byte[] GetBufferAsArray(int length)
+    {
+        return _buffer.AsSpan(0, length).ToArray();
+    }
+}
