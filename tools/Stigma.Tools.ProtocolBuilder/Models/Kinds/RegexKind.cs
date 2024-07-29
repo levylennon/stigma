c0f397ea107b83211ee4a5965502d46c2aaa9cdd
Aerafal <jimmy.mll@hotmail.com>
2024-07-30 01:08:32 +0200

    feat(protocol-builder): add some regexes

--- tools/Stigma.Tools.ProtocolBuilder/Models/Kinds/RegexKind.cs
+++ tools/Stigma.Tools.ProtocolBuilder/Models/Kinds/RegexKind.cs
@@ -4,6 +4,19 @@ public enum RegexKind
 {
     Metadata,
     Namespace,
+    Import,
     EnumProperty,
+    PropertyPrimitive,
+    PropertyObject,
+    PropertyVector,
+    PropertyVectorFieldWriteLength,
+    PropertyVectorFieldWriteMethod,
+    ReadMethodPrimitive,
+    ReadVectorMethodObject,
+    ReadVectorMethodProtocolManager,
+    ReadMethodObjectProtocolManager,
+    VarVectorLimitLength,
+    ReadFlagMethod,
+    ProtocolId,
     ThrowError
 }
--- tools/Stigma.Tools.ProtocolBuilder/Storages/Regexes/RegexStorage.cs
+++ tools/Stigma.Tools.ProtocolBuilder/Storages/Regexes/RegexStorage.cs
@@ -26,7 +26,20 @@ public sealed partial class RegexStorage : IRegexStorage
         {
             RegexKind.Metadata => Metadata(),
             RegexKind.Namespace => Namespace(),
+            RegexKind.Import => Import(),
             RegexKind.EnumProperty => EnumProperty(),
+            RegexKind.PropertyPrimitive => PropertyPrimitive(),
+            RegexKind.PropertyObject => PropertyObject(),
+            RegexKind.PropertyVector => PropertyVector(),
+            RegexKind.PropertyVectorFieldWriteLength => PropertyVectorFieldWriteLength(),
+            RegexKind.PropertyVectorFieldWriteMethod => PropertyVectorFieldWriteMethod(),
+            RegexKind.ReadMethodPrimitive => ReadMethodPrimitive(),
+            RegexKind.ReadVectorMethodObject => ReadVectorMethodObject(),
+            RegexKind.ReadVectorMethodProtocolManager => ReadVectorMethodProtocolManager(),
+            RegexKind.ReadMethodObjectProtocolManager => ReadMethodObjectProtocolManager(),
+            RegexKind.VarVectorLimitLength => VarVectorLimitLength(),
+            RegexKind.ReadFlagMethod => ReadFlagMethod(),
+            RegexKind.ProtocolId => ProtocolId(),
             RegexKind.ThrowError => ThrowError(),
             _ => throw new ArgumentOutOfRangeException(nameof(type))
         };
@@ -38,9 +51,50 @@ public sealed partial class RegexStorage : IRegexStorage
 
     [GeneratedRegex(@"package\s+(?<name>[\w|\.]+)", RegexOptions.Multiline)]
     private static partial Regex Namespace();
-    
+
+    [GeneratedRegex(@"import\s+(?<name>[\w|\.]+)\s*;", RegexOptions.Multiline)]
+    private static partial Regex Import();
+
     [GeneratedRegex(@"public\s+static\s+const\s+(?<name>[\w|_]+):(?<type>[\w]+)\s+=\s+(?<value>[-+\d\w]+);", RegexOptions.Multiline)]
     private static partial Regex EnumProperty();
+
+    [GeneratedRegex(@"public\s+var\s+(?<name>[\w]+)\s*:\s*(?<type>String|Boolean|int|Number|uint|byte|ByteArray)", RegexOptions.Multiline)]
+    private static partial Regex PropertyPrimitive();
+
+    [GeneratedRegex(@"public\s+var\s+(?<name>[\w]+):(?!(String|Boolean|int|Number|uint|byte|ByteArray|Vector)\b)(?<type>[\w]+)", RegexOptions.Multiline)]
+    private static partial Regex PropertyObject();
+
+    [GeneratedRegex(@"public\s+var\s+(?<name>\w+):Vector\.<(?<type>\w+)>;", RegexOptions.Multiline)]
+    private static partial Regex PropertyVector();
+
+    [GeneratedRegex(@"(output|param1)\.(?<method>\w+)\(this\.(?<name>\w+)\.length\);", RegexOptions.Multiline)]
+    private static partial Regex PropertyVectorFieldWriteLength();
+
+    [GeneratedRegex(@"(output|param1)\.(?<method>\w+)\(this.(?<name>\w+)\[", RegexOptions.Multiline)]
+    private static partial Regex PropertyVectorFieldWriteMethod();
+
+    [GeneratedRegex(@"this.(?<name>\w+)\s*=\s*(input|param1)\.(?<method>\w+)\(\);", RegexOptions.Multiline)]
+    private static partial Regex ReadMethodPrimitive();
+
+    [GeneratedRegex(@"_\w+\s*=\s*new\s+(?<type>[\w]+)\(\)\s*;\s*\n\s*_\w+.deserialize\((input|param1)\)\s*;\s*\n\s*this.(?<name>[\w]+).push\(_\w+\)\s*;", RegexOptions.Multiline)]
+    private static partial Regex ReadVectorMethodObject();
+
+    [GeneratedRegex(
+        @"while\(.*\n\s*.*\s*.*(input|param1)\.readUnsignedShort\(\)\);\n\s.*=\s*ProtocolTypeManager\.getInstance\((?<type>\w+),.*\n\s*.*\.deserialize\((input|param1)\);\n\s*this\.(?<name>\w+)\.push",
+        RegexOptions.Multiline)]
+    private static partial Regex ReadVectorMethodProtocolManager();
+
+    [GeneratedRegex(@"this.(?<name>[\w]+)\s*=\s*ProtocolTypeManager.getInstance\(\s*(?<type>[\w]+)", RegexOptions.Multiline)]
+    private static partial Regex ReadMethodObjectProtocolManager();
+
+    [GeneratedRegex(@"while.*<\s*(?<value>[\d]+).*\n\s*.*\n.*this.(?<name>[\w]+)\[", RegexOptions.Multiline)]
+    private static partial Regex VarVectorLimitLength();
+
+    [GeneratedRegex(@"this.(?<name>[\w]+)\s*=\s*BooleanByteWrapper\.getFlag\(_[\w\d]+,\s*(?<flag>[\d]+)\)")]
+    private static partial Regex ReadFlagMethod();
+
+    [GeneratedRegex(@"public\s*static\s*const\s*protocolId:uint\s*=\s*(?<value>[\w]+)\s*;", RegexOptions.Multiline)]
+    private static partial Regex ProtocolId();
 
     [GeneratedRegex(@"throw\s+new\s+Error\s+\(")]
     private static partial Regex ThrowError();
