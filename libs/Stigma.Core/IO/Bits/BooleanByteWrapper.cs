070699f70a7da332187f3d707d544c684436d45a
Aerafal <jimmy.mll@hotmail.com>
2024-07-30 01:12:58 +0200

    feat(core): add boolean byte wrapper

---
+++ libs/Stigma.Core/IO/Bits/BooleanByteWrapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Stigma.Core.IO.Bits;
+
+public static class BooleanByteWrapper
+{
+    public static byte SetFlag(byte flag, byte offset, bool value)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(offset, 8, nameof(offset));
+
+        return value ? (byte)(flag | (1 << offset)) : (byte)(flag & (byte.MaxValue - (1 << offset)));
+    }
+
+    public static bool GetFlag(byte flag, byte offset)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(offset, 8, nameof(offset));
+
+        return (flag & (byte)(1 << offset)) is not 0;
+    }
+}
