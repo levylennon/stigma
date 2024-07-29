77ce5a7226397a14d10b4d3e148236ef140b58e2
Aerafal <jimmy.mll@hotmail.com>
2024-07-30 01:15:57 +0200

    feat(core): add dofus type and dofus message base class

---
+++ libs/Stigma.Core/Network/Metadata/DofusMessage.cs
@@ -0,0 +1,14 @@
+﻿using Stigma.Core.IO.Binary;
+
+namespace Stigma.Core.Network.Metadata;
+
+public abstract class DofusMessage
+{
+    public const uint ProtocolMessageId = 0;
+    
+    public abstract uint ProtocolId { get; }
+    
+    public abstract void Serialize(BigEndianWriter writer);
+    
+    public abstract void Deserialize(BigEndianReader reader);
+}
---
+++ libs/Stigma.Core/Network/Metadata/DofusType.cs
@@ -0,0 +1,14 @@
+﻿using Stigma.Core.IO.Binary;
+
+namespace Stigma.Core.Network.Metadata;
+
+public abstract class DofusType
+{
+    public const ushort ProtocolTypeId = 0;
+    
+    public abstract ushort ProtocolId { get; }
+    
+    public abstract void Serialize(BigEndianWriter writer);
+    
+    public abstract void Deserialize(BigEndianReader reader);
+}
