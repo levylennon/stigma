0872625247be2c5abc899d318102b945c9c9e282
Aerafal <jimmy.mll@hotmail.com>
2024-07-30 01:15:40 +0200

    feat(core): add message factory contract

---
+++ libs/Stigma.Core/Network/Factory/IMessageFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Stigma.Core.Network.Metadata;
+
+namespace Stigma.Core.Network.Factory;
+
+public interface IMessageFactory
+{
+    bool TryCreateInstance(uint id, [NotNullWhen(true)] out DofusMessage? message);
+}
