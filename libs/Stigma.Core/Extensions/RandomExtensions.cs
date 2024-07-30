8163b24d5fd67a2bb4cc09e62a0b333376d80662
Aerafal <jimmy.mll@hotmail.com>
2024-07-30 02:54:39 +0200

    feat(core): add random string extension

---
+++ libs/Stigma.Core/Extensions/RandomExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Stigma.Core.Extensions;
+
+public static class RandomExtensions
+{
+    public static string NextString(this Random random, int length)
+    {
+        var builder = new StringBuilder();
+        
+        for (var i = 0; i < length; i++)
+            builder.Append((char)Math.Floor(26 * random.NextDouble() + 65));
+
+        return builder.ToString();
+    }
+}
