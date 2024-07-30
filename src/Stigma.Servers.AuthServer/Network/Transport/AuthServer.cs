3bc70d67599f865b44c960d2a1b07c229945c2ee
Aerafal <jimmy.mll@hotmail.com>
2024-07-30 02:55:41 +0200

    feat(auth): add auth server and session

---
+++ src/Stigma.Servers.AuthServer/Network/Transport/AuthServer.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using Microsoft.Extensions.Options;
+using Stigma.Core.Network.Dispatcher;
+using Stigma.Core.Network.Framing;
+using Stigma.Core.Network.Options;
+using Stigma.Core.Network.Transport;
+
+namespace Stigma.Servers.AuthServer.Network.Transport;
+
+public sealed class AuthServer : BaseServer<AuthSession>
+{
+    public AuthServer(IMessageParser messageParser, IMessageDispatcher messageDispatcher, IOptions<ServerOptions> options) : base(messageParser, messageDispatcher, options)
+    {
+    }
+
+    protected override AuthSession CreateSession(Socket socket, IMessageParser messageParser, IMessageDispatcher messageDispatcher)
+    {
+        return new AuthSession(socket, messageParser, messageDispatcher);
+    }
+}
---
+++ src/Stigma.Servers.AuthServer/Network/Transport/AuthSession.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using Stigma.Core.Extensions;
+using Stigma.Core.Network.Dispatcher;
+using Stigma.Core.Network.Framing;
+using Stigma.Core.Network.Transport;
+
+namespace Stigma.Servers.AuthServer.Network.Transport;
+
+public sealed class AuthSession : BaseSession
+{
+    public string Ticket { get; }
+    
+    public AuthSession(Socket socket, IMessageParser messageParser, IMessageDispatcher messageDispatcher) : base(socket, messageParser, messageDispatcher)
+    {
+        Ticket = Random.Shared.NextString(32);
+    }
+}
